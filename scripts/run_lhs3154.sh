4b2dbab4d3a89084ee1f188d5e2d361633f714e2
gummiks <gummiks@gmail.com>
2023-10-01 15:11:02 -0400

    update script

--- scripts/run_lhs3154.sh
+++ scripts/run_lhs3154.sh
@@ -1,2 +1,1 @@
-#python ../src/hpfserval.py --npass 1 --inputfolder ../input_data/lhs_3154/ --foldername results_example --vref 9.609 LHS_3154
-python ../src/hpfserval.py --npass 1 --inputfolder ../input_data/lhs_3154_final/ --foldername results --vref 9.609 LHS_3154
+python ../src/hpfserval.py --npass 1 --inputfolder ../input_data/lhs_3154_spectra/ --foldername results --vref 9.609 LHS_3154
